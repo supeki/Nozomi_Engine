5785bb49824183241379ba901594c65426f503a9
Marilyn Nozomi <marilyn@nozomicast.dev>
2026-08-15 02:41:59 -0400

    bugfix so stuff compiles on all plats

--- src/game_misc.c
+++ src/game_misc.c
@@ -11,9 +11,9 @@ char *va(const char *fmt, ...) {
     static int idx = 0;
     va_list ap;
 
-    idx = (idx + 1) & 3; // Rotate through 4 buffers (0 to 3)
+    idx = (idx + 1) & 3;
     va_start(ap, fmt);
-    _vsnprintf(buf[idx], 256, fmt, ap);
+    vsnprintf(buf[idx], 256, fmt, ap);
     va_end(ap);
 
     return buf[idx];
